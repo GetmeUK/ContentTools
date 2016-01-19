b9369f2ac1bbee0d21d5957e808ba12727f9beec
Anthony Blackshaw <ant@getme.co.uk>
2016-01-19 13:18:17 +0000

    Added tests for ImageDialog (incomplete).

--- spec/content-tools-spec.js
+++ spec/content-tools-spec.js
@@ -1000,6 +1000,46 @@
     });
   });
 
+  describe('ContentTools.ImageDialog', function() {
+    var div, editor;
+    div = null;
+    editor = null;
+    beforeEach(function() {
+      div = document.createElement('div');
+      div.setAttribute('class', 'editable');
+      document.body.appendChild(div);
+      editor = ContentTools.EditorApp.get();
+      return editor.init('.editable');
+    });
+    afterEach(function() {
+      editor.destroy();
+      return document.body.removeChild(div);
+    });
+    describe('ContentTools.ImageDialog()', function() {
+      return it('should return an instance of a ImageDialog', function() {
+        var dialog;
+        dialog = new ContentTools.ImageDialog();
+        return expect(dialog instanceof ContentTools.ImageDialog).toBe(true);
+      });
+    });
+    return describe('ContentTools.ImageDialog.cropRegion()', function() {
+      return it('should return the crop region set by the user', function() {
+        var dialog;
+        dialog = new ContentTools.ImageDialog();
+        editor.attach(dialog);
+        dialog.mount();
+        expect(dialog.cropRegion()).toEqual([0, 0, 1, 1]);
+        dialog._domView.style.width = '400px';
+        dialog._domView.style.height = '400px';
+        dialog.populate('test.png', [400, 400]);
+        dialog.addCropMarks();
+        dialog._cropMarks._domHandles[1].style.left = '200px';
+        dialog._cropMarks._domHandles[1].style.top = '200px';
+        return expect(dialog.cropRegion()).toEqual([0, 0, 0.5, 0.5]);
+      });
+    });
+  });
+
   describe('ContentTools.ToolShelf.stow()', function() {
     return it('should store a `ContentTools.Tool` instance against a name', function() {
       var tool;
